ce2dad0cd6c4b4cf3b3f9df80a799dea44aebd83
Otto Urpelainen <oturpe@iki.fi>
2016-06-18 18:03:49 +0200

    Firmware for update schematics; only blinks now however

--- src/Attiny2313Utils.h
+++
@@ -1,4 +0,0 @@
-// Helpers for working with Attiny2313
-
-// Cleaner setting of bits
-#define BV(x) (1<<x)
--- src/config.h
+++ src/config.h
@@ -1,20 +1,18 @@
-// Cpu frequency for util/delay.h
-#define F_CPU 8000000
+// This is the configuration file. Ideally, any configuration and calibration
+// of the device should be done by changing this file only.
 
-// Half of delay in main execution loop, given in microseconds.
-#define LOOP_DELAY 50
+// Needed by util/delay.h
+#define F_CPU 1000000UL
 
-// Half length of indicator led on-off sequence. In other words, the time
-// the indicator is lit and darkened. Given in units of LOOP_DELAY.
-#define INDICATOR_HALF_PERIOD 8000
+// Delay between two executions of main loop, given in millisecond.
+#define LOOP_DELAY 100
 
-// Half length of indicator led on-off sequence when in sensor alarm move. In
-// other words, the time the indicator is lit and darkened. Given in units of
-// LOOP_DELAY.
-#define ALARM_HALF_PERIOD 1200
+// Definition of indicator pin data direction and data ports and pins
+#define INDICATOR_DATA_DIR DDRB
+#define INDICATOR_DATA_DIR_PIN DDB0
+#define INDICATOR_DATA PORTB
+#define INDICATOR_DATA_PIN PORTB0
 
-// Time between stepping the motor. Given in units of LOOP_DELAY
-#define STEP_PERIOD 4
-
-//Enables debug messaging
-#define DEBUG
+// Half length of one complete indicator period. In other words, the time the
+// indicator spends on and off. Given in units of LOOP_DELAY.
+#define INDICATOR_HALF_PERIOD 5
